e212fc3ebaea7aaa5660ae0bfc1596fde450b1f4
Marie <mmdevq@gmail.com>
2024-12-19 13:02:12 -0800

    upd login

---
+++ Views/LoginPage.xaml.cs
@@ -0,0 +1,11 @@
+using ProjectMaui2.ViewModels;
+namespace ProjectMaui2.Views;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage(LoginViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+}
